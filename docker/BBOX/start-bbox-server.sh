7c6150ee48b1ba741f296a53cc8a55cfcf4fce95
Quintin Sparks <quintins@uvic.ca>
2025-01-10 12:12:00 -0800

    Use separate dsn for bbox toml

--- docker/BBOX/start-bbox-server.sh
+++ docker/BBOX/start-bbox-server.sh
@@ -9,6 +9,12 @@ until psql "$DB_DSN" -c "SELECT 1 FROM rivers LIMIT 1;" 2>/dev/null; do
   sleep 2
 done
 
+echo "Reading DB password from secret..."
+POSTGRES_PASSWORD=`cat /run/secrets/bbox-postgis-SU`
+echo "Constructing DSN..."
+BBOX_DB_DSN="postgresql://bbox_user:${POSTGRES_PASSWORD}@postgis:5432/bbox_postgres"
+export BBOX_DB_DSN
+
 echo "Data is ready. Substituting environment variables..."
 envsubst < /tmp/bbox.template.toml > /app/bbox.toml
 
